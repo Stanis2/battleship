b1256a8111344df89277c171884328a2b6e6a253
java_tsi08 <stanis2@inbox.lv>
2018-11-08 19:31:11 +0200

    Initial

---
+++ src/main/java/lv/tsi/battleship/RegServlet.java
@@ -0,0 +1,19 @@
+package lv.tsi.battleship;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "RegServlet", urlPatterns = "/registration")
+public class RegServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("WEB-INF/pages/registration.jsp").include(request, response);
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/registration.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Registration</title>
+</head>
+
+<body>
+        <form method="post" action="/battleship/registration">
+            <label for="username">Enter your username:</label>
+            <input id="username" type="text" name="Username">
+            <br>
+            <button type="submit">Join!</button>
+        </form>
+</body>
+</html>
